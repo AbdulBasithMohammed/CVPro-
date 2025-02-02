f9465df82923a494f114f2cbd86c828192a118dc
Abdul Basith Mohammed <ab430929@dal.ca>
2025-02-02 19:07:29 -0400

    feat(database): mask all database credentials and retrieve credentials from environment variables

--- backend/atsresume/atsresume/settings.py
+++ backend/atsresume/atsresume/settings.py
@@ -9,8 +9,27 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+from urllib.parse import quote_plus
+from dotenv import load_dotenv
+
+def get_creds_from_env():
+    load_dotenv("cred.env") # This must be removed along with cred.env file once environment variables are set in gitlab.
+
+    # Retrieve credentials from environment variables
+    username = os.getenv("MONGO_USERNAME")
+    password = os.getenv("MONGO_PASSWORD")
+    cluster_name = os.getenv("MONGO_CLUSTER_NAME")
+    db_name = os.getenv("MONGO_DB_NAME")
+
+    # Check if any required variable is missing
+    if not username or not password or not cluster_name or not db_name:
+        raise ValueError("One or more required environment variables are missing!")
+
+    return username,password,cluster_name,db_name
+
+mongo_user,mongo_pass,mongo_cluster,mongo_db = get_creds_from_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,14 +105,14 @@ DATABASES = {
     'default': {
         'ENGINE': 'djongo',
         'NAME': 'ats_resume',  # Replace with your MongoDB database name
-        'EN host': 'mongodb+srv://group6asdc:G06asdc@@@cluster0.mongodb.net',  # Replace with your MongoDB Atlas URI
+        'EN host': f'mongodb+srv://{mongo_user}:{mongo_pass}@cluster0.mongodb.net',  # Replace with your MongoDB Atlas URI
         'EN': 'test',  # Optionally, specify a test database
     }
 }
-from urllib.parse import quote_plus
-escaped_username = quote_plus("group6asdc")
-escaped_password = quote_plus("G06asdc@@")
-MONGO_URI=f'mongodb+srv://{escaped_username}:{escaped_password}@g6user-db.7hzwm.mongodb.net/?retryWrites=true&w=majority&appName=g6user-db'
+
+escaped_username = quote_plus(mongo_user)
+escaped_password = quote_plus(mongo_pass)
+MONGO_URI=f'mongodb+srv://{escaped_username}:{escaped_password}@{mongo_cluster}/?retryWrites=true&w=majority&appName=g6user-db'
 
 
 # Password validation
--- backend/atsresume/db_connection.py
+++ backend/atsresume/db_connection.py
@@ -1,11 +1,3 @@
-# import pymongo
-# from urllib.parse import quote_plus
-# escaped_username = quote_plus("group6asdc")
-# escaped_password = quote_plus("G06asdc@@")
-# url=f'mongodb+srv://{escaped_username}:{escaped_password}@g6user-db.7hzwm.mongodb.net/?retryWrites=true&w=majority&appName=g6user-db'
-# client=pymongo.MongoClient(url)
-# db=client['ats_resume']
-
 # mongo_utils.py
 import pymongo
 from django.conf import settings
@@ -14,35 +6,3 @@ def get_mongo_connection():
     client = pymongo.MongoClient(settings.MONGO_URI)
     db = client.get_database("ats_resume")  # Use the database specified in the connection URI
     return db
-
-
-# from pymongo import MongoClient
-# from urllib.parse import quote_plus
-# import os
-# from dotenv import load_dotenv
-
-# load_dotenv("cred.env") # File path must be removed after Gitlab CI/CD Pipeline has environment variables set.
-
-# def get_db_connection():
-#     # Retrieve credentials from environment variables
-#     username = os.getenv("MONGO_USERNAME")
-#     password = os.getenv("MONGO_PASSWORD")
-#     cluster_name = os.getenv("MONGO_CLUSTER_NAME")
-#     db_name = os.getenv("MONGO_DB_NAME")
-
-#     # Check if any required variable is missing
-#     if not username or not password or not cluster_name or not db_name:
-#         raise ValueError("One or more required environment variables are missing!")
-
-#     # Escape special characters in the username and password
-#     escaped_username = quote_plus(username)
-#     escaped_password = quote_plus(password)
-
-#     # Construct the connection string
-#     uri = f"mongodb+srv://{escaped_username}:{escaped_password}@{cluster_name}/?retryWrites=true&w=majority&appName={cluster_name}"
-
-#     # Connect to MongoDB Atlas
-#     client = MongoClient(uri)
-#     db = client[db_name]
-#     print("Connected to MongoDB Atlas successfully!")
-#     return db
