7182bf6d53a338d833dd6336640021f977d6d650
Arshad vhora <arshadvhora512@gmail.com>
2025-02-05 11:40:11 -0400

    update jason dependencies.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Signup from "./pages/signup";
 import Home from "./pages/home";
 import Login from "./pages/login";
 import AboutUs from "./pages/aboutus";
+import ContactUs from "./pages/contactus";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/contactus" element={<ContactUs />} />
       </Routes>
     </Router>
   );
